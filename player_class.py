270159111beb5f7a95836f3ab8b2453178271e11
kungfucoder <35316533+kungfucoder@users.noreply.github.com>
2018-02-11 15:09:44 -0500

    basic start to the characterClass
    Creates a class to hold the characters information. So far only name, age, attributes and skill bonuses have been defined.

---
+++ player_class.py
@@ -0,0 +1,37 @@
+class playerClass():
+
+    #holds information related to the character
+    def _init_(character,name,age):
+
+        #sets the characters name and age
+        character.name = name
+        character.age = age
+
+    def abilityScores(attribute,strength,craft,charisma):
+
+        attribute.strength = strength
+        attribute.craft = craft
+        attribute.charisma = charisma
+
+        def strengthSkills(bonus,intimidate,lift,climb):
+
+            bonus.intimidate = intimidate
+            bonus.lift = lift
+            bonus.climb = climb
+
+        def craftSkills(bonus,appraise,repair,build):
+
+            bonus.appraise = appraise
+            bonus.repair = repair
+            bonus.build = build
+
+        def charismaSkills(bonus,persuade,deceive,rally):
+
+            bonus.persuade = persuade
+            bonus.deceive = deceive
+            bonus.rally = rally
+
+        
+
+
+    
