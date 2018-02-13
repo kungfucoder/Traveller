f4483be4ce8d297b5ab985170fdd67b5ce5c07ac
kungfucoder <35316533+kungfucoder@users.noreply.github.com>
2018-02-12 19:51:12 -0500

    updated the skills
    -Skills can now be called and produce the bonus associated with their related ability score. 
    -After staring at this and toying with the code, this does seem to be the best method as of right now. 
    -I understand that this will not store the associated number with the skills and will require it to be calculated every time it is called. Whether this is actually helpful or a waste of resources, depends on if the ability scores will remain static throughout the entirety of the game, or if they are dynamic and subject to change.

--- player_class.py
+++ player_class.py
@@ -20,25 +20,59 @@ class playerClass():
         print('Your charisma is, ' + charisma + '.')
 
 
-        def strengthSkills(bonus,intimidate,lift,climb):
+       def intimidate(self):
+        return (self.strength - 10) / 2
+        
 
-            bonus.intimidate = intimidate
-            bonus.lift = lift
-            bonus.climb = climb
+    def lift(self):
+        return (self.strength - 10) / 2
 
-        def craftSkills(bonus,appraise,repair,build):
+    def climb(self):
+        return (self.strength - 10) / 2
 
-            bonus.appraise = appraise
-            bonus.repair = repair
-            bonus.build = build
+    def repair(self):
+        return (self.craft - 10) / 2
 
-        def charismaSkills(bonus,persuade,deceive,rally):
+    def build(self):
+        return (self.craft - 10) / 2
 
-            bonus.persuade = persuade
-            bonus.deceive = deceive
-            bonus.rally = rally
+    def appraise(self):
+        return (self.craft - 10) / 2
 
+    def persuade(self):
+        return (self.charisma - 10) / 2
 
-        myAttributes = abilityScores(12,16,13)
+    def deceive(self):
+        return (self.charisma - 10) / 2
 
-myCharacter = playerClass('James', 24)
+    def rally(self):
+        return (self.charisma - 10) / 2
+
+attrs1 = {
+    "name": "James",
+    "age": 24,
+    "strength": 12,
+    "craft": 16,
+    "charisma": 13
+}
+
+attrs2 = {
+    "name": "Matthew",
+    "age": 36,
+    "strength": 14,
+    "craft": 11,
+    "charisma": 16
+}
+
+player1 = playerClass(attrs1)
+player2 = playerClass(attrs2)
+
+player1.abilityScores()
+print player1.intimidate()
+print player1.lift()
+print player1.climb()
+
+player2.abilityScores()
+print player2.intimidate()
+print player2.lift()
+print player2.climb()
